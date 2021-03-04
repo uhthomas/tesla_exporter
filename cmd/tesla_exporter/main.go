31f5cb1cdb0cc49af605ee1cda0543995ef1750e
Thomas <9749173+uhthomas@users.noreply.github.com>
2021-03-04 18:42:36 +0000

    Use sensible HTTP client

--- cmd/tesla_exporter/main.go
+++ cmd/tesla_exporter/main.go
@@ -5,14 +5,18 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/uhthomas/tesla"
 	"github.com/uhthomas/tesla_exporter/internal"
 	"github.com/uhthomas/tesla_exporter/internal/collector"
+	"golang.org/x/oauth2"
 )
 
 func Main(ctx context.Context) error {
@@ -20,6 +24,18 @@ func Main(ctx context.Context) error {
 	oauth2ConfigPath := flag.String("oauth2-config-path", "oauth2_config.json", "Tesla OAuth2 config file")
 	oauth2TokenPath := flag.String("oauth2-token-path", "oauth2_token.json", "Tesla OAuth2 token file")
 	flag.Parse()
+
+	ctx = context.WithValue(ctx, oauth2.HTTPClient, &http.Client{
+		Transport: &http.Transport{
+			Dial: (&net.Dialer{
+				Timeout:   30 * time.Second,
+				KeepAlive: 30 * time.Second,
+			}).Dial,
+			TLSHandshakeTimeout:   10 * time.Second,
+			ResponseHeaderTimeout: 10 * time.Second,
+			ExpectContinueTimeout: 1 * time.Second,
+		},
+	})
 
 	s, err := tesla.New(ctx,
 		tesla.OAuth2(*oauth2ConfigPath, *oauth2TokenPath),
